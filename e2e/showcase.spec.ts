729125c30b1db9466c81028c99e82d252503af8d
er. <145699040+oqzx@users.noreply.github.com>
2026-04-05 16:02:40 +0300

    chore(showcase): resolve issue

--- e2e/showcase.spec.ts
+++ e2e/showcase.spec.ts
@@ -233,7 +233,7 @@ test('capture showcase', async () => {
       const theadH = thead?.offsetHeight ?? 35
       const tbodyH = tbody?.scrollHeight ?? 0
       const footerH = footer?.offsetHeight ?? 34
-      const tableW = (table?.scrollWidth - 5) ?? 0
+      const tableW = table?.scrollWidth ?? 0
 
       return {
         contentW: tableW,
